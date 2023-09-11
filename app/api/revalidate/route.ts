51286e927019f3dd96ce07d2dd9b142b9b1e9afb
Ali Tamer <alitamer82.at@gmail.com>
2023-09-11 17:17:36 +0300

    edge runtime

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { revalidatePath } from 'next/cache'
 
-export const runtime = 'edge'
+// export const runtime = 'edge'
 
 export async function POST(request: Request) {
     const { searchParams } = new URL(request.url)
